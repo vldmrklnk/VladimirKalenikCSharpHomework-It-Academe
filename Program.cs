2963cde9b9183e4da0dd81d46b66028387f79407
vldmrklnk <uladzimerkalenik@gmail.com>
2020-04-04 23:20:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hm6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack<string> students = new Stack<string>();
+			while (true)
+			{
+				Console.WriteLine("1. Чтобы сдать работу введите Send \n"+
+								  "2. Чтобы получить кофе введите Coffee \n");
+				var action = Console.ReadLine();
+				switch (action)
+				{
+					case "Coffee":
+						{
+							try
+							{
+								Console.WriteLine($"{students.Pop()} got a cup of coffee");
+								break;
+							}
+							catch (Exception)
+							{
+								Console.WriteLine("Все работы проверены и кофе роздан. Введите команду:");
+								break;
+							}
+						}
+					case "Send":
+						{
+							Console.WriteLine("Введите свои данные");
+							var name = Console.ReadLine();
+							students.Push(name);
+							break;
+						}
+
+
+				}
+
+			}
+		}
+	}
+}
